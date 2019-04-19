ae88f8933263ebd5e5fae66a887375b81d8219a0
Abdul <awsayed@alaska.edu>
2019-04-19 02:35:30 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,8 +6,4 @@ describe "current_age_for_birth_year method" do
 
     expect(age_of_person).to eq(19)
   end
-  it "returns a positive number" do
-
-  expect(current_age_for_birth_year(1984)).to be(>=0)
-end
 end
