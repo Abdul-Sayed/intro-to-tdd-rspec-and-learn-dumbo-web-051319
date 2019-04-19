84c1444abf7976b1a947cc221165c0a0f647909c
Abdul <awsayed@alaska.edu>
2019-04-19 02:26:47 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,4 +6,9 @@ describe "current_age_for_birth_year method" do
 
     expect(age_of_person).to eq(19)
   end
+  it "returns the age of a person based on the year of birth" do
+  age_of_person = current_age_for_birth_year(1984)
+
+  expect(age_of_person).to eq(19)
+end
 end
